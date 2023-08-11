4d019df0ad459dca15d2fca209f0ffdb0ce089db
Alexander Ignatev <alexign473@gmail.com>
2023-08-11 18:00:52 +0300

    types App index

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import { Header } from './components/Header';
+// @ts-ignore
+import { Main } from './components/Main';
+// @ts-ignore
+import data from './data';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Header />
+      <Main data={data} />
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ src/types.d.ts
@@ -0,0 +1,16 @@
+interface IPersonal {
+  name: string;
+  email: string;
+  phone: string;
+  institution: string;
+  spec: string;
+  gradDate: string;
+}
+
+interface IExp {
+  company: string;
+  position: string;
+  from: string;
+  to: string;
+  description: string;
+}
