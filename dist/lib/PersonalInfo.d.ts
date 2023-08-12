c365c9a7c3de9505564a20c4582e1277e3084552
Alexander Ignatev <alexign473@gmail.com>
2023-08-12 19:04:22 +0300

    rm dist

--- dist/lib/PersonalInfo.d.ts
+++
@@ -1,6 +0,0 @@
-declare type PersonalInfoProps = {
-    personal: any;
-};
-export declare const PersonalInfo: ({ personal, onSave, onEdit, stopEditing, editIdx, }: PersonalInfoProps) => import("react/jsx-runtime").JSX.Element;
-export {};
-//# sourceMappingURL=PersonalInfo.d.ts.map
--- dist/lib/common/InfoWrapper.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-declare type Props = {
-    title: string;
-    children?: React.ReactNode;
-};
-export declare const InfoWrapper: ({ title, children, ...props }: Props) => import("react/jsx-runtime").JSX.Element;
-export {};
-//# sourceMappingURL=InfoWrapper.d.ts.map
